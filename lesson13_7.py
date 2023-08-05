d7f657823f612cf37402d8937673711587fd117c
Matt0612 <136140379+Matt0612@users.noreply.github.com>
2023-08-05 08:26:46 +0000

    AK

--- lesson13_7.py
+++ lesson13_7.py
@@ -2,6 +2,11 @@
 import pandas as pd
 import streamlit as st
 
-current_weather = pd.red_csv('目前天氣.csv')
-st.write('目前泰灣天氣')
+current_weather = pd.read_csv('目前天氣.csv')
+st.write('目前台灣天氣')
 st.write(current_weather)
+
+
+#st.write()為上傳網頁使用
+#在終端機輸入指令,streamlit run 檔名.py
+#ctrl+C為中止功能
