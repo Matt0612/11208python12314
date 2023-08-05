ebe6013ca6ad740a2df55dbc5d68955a205092b8
Matt0612 <136140379+Matt0612@users.noreply.github.com>
2023-08-05 05:44:46 +0000

    AF

---
+++ lesson13_3_3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+value = st.slider("三角函式",min_value=0,max_value=5)
+t = np.arange(0.,5,0.05)
+y1 = np.sin(np.random.randn() * value * np.pi * t)
+y2 = np.cos(np.random.randn()* value * np.pi * t)
+figure1 = plt.figure(figsize=(8,4))
+axes1 = figure1.add_subplot()
+axes1.plot(y1)
+axes1.plot(y2)
+st.write(figure1)
+
+
+#st.write()為上傳網頁使用
+#在終端機輸入指令,streamlit run 檔名.py
+#ctrl+C為中止功能
